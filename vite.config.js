186f13f1854e6e2f758e9905c003eb46c966828d
HimajaHimaja <gandlahimaja14@gmaial.com>
2025-07-29 11:25:38 +0530

    version 1.1

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/cicd-first-project/',
+  base: '/CICD-First-Project/',
   plugins: [react()],
 })
